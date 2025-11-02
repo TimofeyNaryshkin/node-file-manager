4368166ec32aa6e4d0105b2b5e3c1d2f56310275
Timofey Naryshkin <naryshkint4@gmail.com>
2025-11-02 23:14:59 +0900

    feat: implement list function

--- src/index.js
+++ src/index.js
@@ -2,28 +2,54 @@ import { stdin, stdout } from "node:process";
 import { greet, bye } from "./greet.js";
 import { homedir } from "node:os";
 import readline from "node:readline";
+import list from "./list.js";
+
+const curDirMessage = () => {
+  console.log(`You are currently in ${workDir}`);
+};
+
+export let workDir = homedir();
 
 greet();
-curDir();
-let workDir = homedir();
-const curDir = () => {
-  stdout.write(`You are currently in ${import.meta.dirname}`);
+curDirMessage();
+
+const showInvalidMessage = () => {
+  console.log("Invalid input");
 };
-const errorMessage = () => {
-  stdout.write("Invalid input");
+
+const goUp = () => {
+  const lastSlashIndex = workDir.lastIndexOf("\\");
+  const slashIndex = workDir.indexOf("\\");
+
+  if (lastSlashIndex !== -1) {
+    if (lastSlashIndex !== slashIndex) {
+      workDir = workDir.slice(0, lastSlashIndex);
+    } else {
+      workDir = workDir.slice(0, lastSlashIndex) + "\\";
+    }
+  }
 };
 
 const rl = readline.createInterface({ input: stdin, output: stdout });
 
-rl.on("line", (input) => {
-  switch (input) {
+rl.on("line", async (input) => {
+  const inputArr = input.split(" ");
+  const command = inputArr[0];
+
+  switch (command) {
+    case "up":
+      goUp();
+      break;
+    case "ls":
+      await list();
+      break;
     case ".exit":
       rl.close();
       break;
     default:
-      errorMessage();
+      showInvalidMessage();
   }
-  curDir();
+  curDirMessage();
 });
 
 rl.on("close", bye);
