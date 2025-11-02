5aed01c3524fe9688eb5d4440a1a7091b75e70ca
Timofey Naryshkin <naryshkint4@gmail.com>
2025-11-02 21:54:19 +0900

    feat: add program close handler, error message and working directory

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import { stdin, stdout } from "node:process";
 import { greet, bye } from "./greet.js";
+import { homedir } from "node:os";
 import readline from "node:readline";
 
 greet();
+curDir();
+let workDir = homedir();
+const curDir = () => {
+  stdout.write(`You are currently in ${import.meta.dirname}`);
+};
+const errorMessage = () => {
+  stdout.write("Invalid input");
+};
 
 const rl = readline.createInterface({ input: stdin, output: stdout });
 
 rl.on("line", (input) => {
   switch (input) {
     case ".exit":
-      bye();
+      rl.close();
       break;
+    default:
+      errorMessage();
   }
+  curDir();
 });
+
+rl.on("close", bye);
